6f5bd434bcf6d0bf8f3a9b806483fa8ba2e79fed
VilleValtteri <villekoivunen_5@hotmail.com>
2022-04-28 22:08:04 +0300

    Changes to search algorithm

--- main.py
+++ main.py
@@ -5,30 +5,27 @@ from tkinter import ttk
 class PlayingFieldFrame(ttk.Frame):
     def __init__(self, container):
         super().__init__(container)
-
-
-class InputFrame(ttk.Frame):
-    def __init__(self, container):
-        super().__init__(container)
         self._create_widgets()
 
     def _create_widgets(self):
-        pass
+        for i in range(11):
+            for j in range(11):
+                b = tk.Button(text="", height=2, width=5)
+                b.grid(row=i, column=j)
 
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("A* search")
-        self.geometry("500x500")
+        self.geometry("495x500")
         self.resizable(0, 0)
 
-    def _create_widgets(self):
-        pass
 
 
 if __name__ == "__main__":
     app = App()
+    PlayingFieldFrame(app)
     app.mainloop()
 
 
--- searchAlgorithm.py
+++ searchAlgorithm.py
@@ -2,34 +2,42 @@ from typing import List, Tuple
 from userExceptions import BoardEmpty
 
 
+def heuristic(y: int, x: int, y2: int, x2: int) -> int:
+    return abs(y2-y) + abs(x2-x)
+
+
 def check_valid_cell(y: int, x: int, board: List[List[int]]) -> bool:
     valid_y: bool = (0 <= y < len(board))
     valid_x: bool = (0 <= x < len(board[0]))
     if valid_y and valid_x:
-        return board[valid_y][valid_x] == 0
+        return board[y][x] == 0
     return False
 
 
-def expand_neighbours(best_option: List[int], board: List[List[int]], open_points: List[List[int]]) -> None:
+def add_to_open(y: int, x: int, h: int, g: int, open: List[List[int]], board: List[List[int]]) -> Tuple[List[List[int]], List[List[int]]]:
+    open.append([y, x, h, g])
+    board[y][x] = 4
+    return open, board
+
+
+def expand_neighbours(best_option: List[int], board: List[List[int]], open_points: List[List[int]], goal: Tuple[int, int]) -> List[List[int]]:
     delta: List[Tuple[int, int]] = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     y = best_option[0]
     x = best_option[1]
-    h = best_option[2]
     g = best_option[3]
     for i in range(len(delta)):
         new_y = y + delta[i][0]
         new_x = x + delta[i][1]
         if check_valid_cell(new_y, new_x, board):
-
+            new_h = heuristic(new_y, new_x, goal[0], goal[1])
+            new_g = g + 1
+            open_points, board = add_to_open(new_y, new_x, new_h, new_g, open_points, board)
+    return open_points
 
 
 def sort_points(open_points: List[List[int]]) -> List[List[int]]:
     open_points.sort(key=lambda x: x[2] + x[3], reverse=True)
     return open_points
-
-
-def heuristic(y: int, x: int, y2: int, x2: int) -> int:
-    return abs(y2-y) + abs(x2-x)
 
 
 def search(board: List[List[int]], start: Tuple[int, int], end: Tuple[int, int]) -> List[List[int]]:
@@ -38,7 +46,7 @@ def search(board: List[List[int]], start: Tuple[int, int], end: Tuple[int, int])
     x = start[1]
     h = heuristic(y, x, end[0], end[1])
     g = 0
-    open_points.append([y, x, h, g])
+    open_points, board = add_to_open(y, x, h, g, open_points, board)
     while not is_board_empty(open_points):
         open_points = sort_points(open_points)
         best_option: List[int] = open_points.pop()
@@ -48,8 +56,8 @@ def search(board: List[List[int]], start: Tuple[int, int], end: Tuple[int, int])
         if new_y == end[0] and new_x == end[1]:
             board[end[0]][end[1]] = 2
             return board
-        expand_neighbours(best_option, board, open_points)
-
+        open_points = expand_neighbours(best_option, board, open_points, end)
+    return [[1]]
 
 def print_board(board: List[List[int]]) -> None:
     for i in range(len(board)):
@@ -83,4 +91,7 @@ def load_board(path: str) -> List[List[int]]:
 
 path: str = "./map.txt"
 mapf = load_board(path)
-print_board(mapf)
+start = (0, 0)
+end = (5, 6)
+k = search(mapf, start, end)
+print_board(k)
